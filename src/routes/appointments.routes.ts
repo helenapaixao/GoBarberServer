34492f94a8383319846ac2412d37793b181e18ed
Helena Paixão <hp.helenapaixao@gmail.com>
2020-05-01 12:38:45 -0300

    add models page appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,26 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository;
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-const appointments: Appointment[] = [];
+
+
+
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if (findAppointmentInSameDate) {
     return response.status(400).json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
