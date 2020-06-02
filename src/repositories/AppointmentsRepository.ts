478b8a01b20b486f68b6377f28b166680ef621cd
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-02 16:00:13 -0400

    add container,config Typerorm, create table appointment and create model

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,34 +1,15 @@
-import { isEqual } from 'date-fns';
+
 import Appointment from '../models/Appointment';
+import { EntityRepository, Repository } from 'typeorm';
 
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public  async findByDate(date: Date): Promise<Appointment | null>{
+    const findAppointment =  await this.findOne({
+      where: {  date}
+    });
 
-interface CreateAppointmentDTO {
-
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment => isEqual(date, appointment.date));
     return findAppointment || null;
-  }
-
-  public create({provider,date}: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({provider, date});
-
-    this.appointments.push(appointment);
-    return appointment;
   }
 }
 
