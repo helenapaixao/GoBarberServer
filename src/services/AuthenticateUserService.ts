07f5929c3fee3070a5efda68ab5260968c9af903
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-22 18:18:52 -0400

    gerando token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,15 +1,16 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
-import {compare} from 'bcryptjs'
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface Request {
   email: string;
   password: string;
 }
 
-
 interface Response {
-  user: User
+  user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -21,16 +22,20 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-
-
     const passwordMatched = await compare(password, user.password);
-    if(!passwordMatched) {
-      throw new Error('Incorrect email/password combination')
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
     }
+
+    const token = sign({}, '7ca5cb0c0de012607dffec988e749ee1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
-    }
+      token
+    };
   }
 }
 
