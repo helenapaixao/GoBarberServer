0e533d42fc8f26075b5d5d67bb788778c44121ba
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-25 21:43:45 -0400

    config routes

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import Appointment from '../../../models/Appointment';
-import AppointmentsRepository from '../../../repositories/AppointmentsRepository';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
