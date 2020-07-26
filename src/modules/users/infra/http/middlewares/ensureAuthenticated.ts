bc8025518017fff39c744e3c90c5f1d721b0d9bf
Helena Paixão <hp.helenapaixao@gmail.com>
2020-07-25 21:33:22 -0400

    config ensure

--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -19,7 +19,7 @@ export default function ensureAuthenticated(
   const autHeader = request.headers.authorization;
 
   if (!autHeader) {
-    throw new Error('JWT token is missing.');
+    throw new AppError('JWT token is missing.');
   }
 
   const [, token] = autHeader.split(' ');
@@ -34,6 +34,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch (err) {
-    throw new Error('Invalid token.');
+    throw new AppError('Invalid token.');
   }
 }
