69f22bfb6a56ba4a7d9bef66c137c663cf563289
Helena Paixão <hp.helenapaixao@gmail.com>
2020-08-31 19:12:10 -0400

    provider storage: ok

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,13 +1,12 @@
-
 import path from 'path';
 import User from '../infra/typeorm/entities/User';
 import uploadConfig from '@config/upload';
-import {injectable, inject} from 'tsyringe';
-
+import { injectable, inject } from 'tsyringe';
 
 import fs from 'fs';
-import AppError from'@shared/errors/AppError'
-import IUsersRepository from '../repositories/IUsersRepository'
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 interface IRequest {
   user_id: string;
   avatarFilename: string;
@@ -16,27 +15,24 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository,) {}
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) {}
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
-
-
     const user = await this.usersRepository.findById(user_id);
     if (!user) {
       throw new AppError('Only authenticated users can change avatar.');
     }
 
     if (user.avatar) {
-      // Deletar avatar anterior
-
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
     await this.usersRepository.save(user);
     return user;
   }
---
+++ src/shared/container/providers/StorageProvider/models/IStorageProvider.ts
@@ -0,0 +1,7 @@
+
+
+export default interface IStorageProvider {
+  saveFile(file:string): Promise<string>;
+  deleteFile(file:string): Promise<void>;
+
+}
