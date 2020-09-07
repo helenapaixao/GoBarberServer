b30fe92f4ca9751176da03f420f148bb0fa6e866
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-06 21:46:35 -0400

    email em desenvolvimento

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -33,7 +33,7 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
     await this.userTokesRepository.generate(user.id);
-    this.mailProvider.sendEmail(
+    await this.mailProvider.sendEmail(
       email,
       'Pedido de recuperação de senha recebido',
     );
