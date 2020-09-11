f00787fbb616e7d19aa0c614b2f11374cfeb62d7
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-11 15:45:20 -0400

    validando dados com joi

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import cors from 'cors'
+import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import '../typeorm';
@@ -17,22 +18,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
+app.use(errors());
 
-    console.log(err);
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  console.log(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
