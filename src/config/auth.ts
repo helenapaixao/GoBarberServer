31ab2b733896443ce8ddc8daf4ad06b5bb1d51c1
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-11 16:39:04 -0400

    add variaveis de ambiente

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,15 +1,9 @@
-
 interface AuthConfig {
   secret: string;
   expiresIn: string;
 }
 
-
 export default {
-
-    secret: '7ca5cb0c0de012607dffec988e749ee1',
-    expiresIn: '1d'
-
-}as AuthConfig;
-
-
+  secret: process.env.APP_SECRET,
+  expiresIn: '1d',
+} as AuthConfig;
