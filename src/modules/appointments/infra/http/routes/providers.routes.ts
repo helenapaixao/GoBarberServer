86fec63d56513d23a137cfe45cc563ab29a31b63
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-11 15:49:14 -0400

    validando dados providers routes

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import ProvidersController from '../controllers/ProvidersController';
+import {celebrate,Segments, Joi} from 'celebrate'
 
 import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
@@ -8,14 +9,24 @@ import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailab
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
 const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
+providersRouter.get('/:provider_id/month-availability',celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  },
+}),
+ providerMonthAvailabilityController.index);
+providersRouter.get('/:provider_id/day-availability',celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  },
+}), providerDayAvailabilityController.index);
 
 
 export default providersRouter;
