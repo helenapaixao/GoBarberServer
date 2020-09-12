51e0336d9f76a09696ae6e63ffcd735a570b9f87
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-12 18:10:12 -0400

    fix: agendamento no mesmo horário

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,64 +1,18 @@
-import Appointment from '../entities/Appointment';
+
 import { getRepository, Repository, Raw } from 'typeorm';
-import { startOfMonth, endOfMonth } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
+
+import Appointment from '../entities/Appointment';
+
 class AppointmentsRepository implements IAppointmentsRepository {
   private ormRepository: Repository<Appointment>;
+
   constructor() {
     this.ormRepository = getRepository(Appointment);
-  }
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.ormRepository.findOne({
-      where: { date },
-    });
-
-    return findAppointment;
-  }
-
-  public async findAllInMonthFromProvider({
-    month,
-    provider_id,
-    year,
-  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
-    const parsedMonth = String(month).padStart(2, '0');
-
-    const appointments = await this.ormRepository.find({
-      where: {
-        provider_id,
-        date: Raw(
-          dateFieldName =>
-            `to_char(${dateFieldName}, ''MM-YYYY) = '${parsedMonth}-${year}'`,
-        ),
-      },
-    });
-
-    return appointments;
-  }
-
-  public async findAllInDayFromProvider({
-    month,
-    day,
-    provider_id,
-    year,
-  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
-    const parsedDay = String(day).padStart(2, '0');
-    const parsedMonth = String(month).padStart(2, '0');
-
-    const appointments = await this.ormRepository.find({
-      where: {
-        provider_id,
-        date: Raw(
-          dateFieldName =>
-            `to_char(${dateFieldName}, 'DD-MM-YYYY) = '${parsedDay}-${parsedMonth}-${year}'`,
-        ),
-      },
-    });
-
-    return appointments;
   }
 
   public async create({
@@ -68,11 +22,71 @@ class AppointmentsRepository implements IAppointmentsRepository {
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = this.ormRepository.create({
       provider_id,
-      user_id,
       date,
+      user_id,
     });
+
     await this.ormRepository.save(appointment);
+
     return appointment;
+  }
+
+  public async findByDateFromProvider(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date, provider_id },
+    });
+
+    return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    year,
+    month,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = this.getTwoDigitsVersionOf(month);
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id: provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = this.getTwoDigitsVersionOf(day);
+    const parsedMonth = this.getTwoDigitsVersionOf(month);
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id: provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+        ),
+      },
+      relations: ['user'],
+    });
+
+    return appointments;
+  }
+
+  private getTwoDigitsVersionOf(num: number): string {
+    return String(num).padStart(2, '0');
   }
 }
 
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,47 +1,61 @@
-import { startOfHour, isBefore, getHours, format} from 'date-fns';
+
 import { injectable, inject } from 'tsyringe';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
+
 import AppError from '@shared/errors/AppError';
 
-import Appointment from '../infra/typeorm/entities/Appointment';
+/* import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'; */
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
-import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+
 interface IRequest {
   provider_id: string;
   user_id: string;
   date: Date;
 }
+
 @injectable()
-class CreateAppointmentService {
+export default class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+   /*  @inject('CacheProvider')
+    private cacheProvider: ICacheProvider, */
   ) {}
 
   public async execute({
-    date,
     provider_id,
     user_id,
+    date,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     if (isBefore(appointmentDate, Date.now())) {
-      throw new AppError('You cant create an appointment on past date');
+      throw new AppError("You can't create an appointment on a past date.");
     }
 
     if (user_id === provider_id) {
-      throw new AppError('You cant create an appointment with yourself');
+      throw new AppError("You can't create an appointment with yourself.");
     }
 
-    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+    const appointmentHour = getHours(appointmentDate);
+
+    if (appointmentHour < 8 || appointmentHour > 17) {
       throw new AppError(
-        'You cant only create appointments between 8am and 5pm',
+        'You can only create appointments between 8am and 5pm',
       );
     }
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDateFromProvider(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
@@ -54,15 +68,23 @@ class CreateAppointmentService {
       date: appointmentDate,
     });
 
-    const dateFormated = format(appointmentDate, "dd/MM/yyyy 'ás' HH:mm'h'");
-
+    const formatedDateString = format(
+      appointmentDate,
+      "dd/MM/yyy 'às' HH:mm'h'",
+    );
 
     await this.notificationsRepository.create({
       recipient_id: provider_id,
-      content:`Novo agendamento para dia ${dateFormated}`,
-    })
+      content: `Novo agendamento para o dia ${formatedDateString}`,
+    });
+
+/*     await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d',
+      )}`,
+    ); */
+
     return appointment;
   }
 }
-
-export default CreateAppointmentService;
